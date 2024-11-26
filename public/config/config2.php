50e46dd48807608dc5f19ce5d4019a26b5e3afa9
GUSTAVO SALERNO <91195618+KriptonProgramer@users.noreply.github.com>
2024-11-26 16:04:34 -0300

    Update config2.php

--- public/config/config2.php
+++ public/config/config2.php
@@ -1,19 +1,14 @@
 <?php
 // Datos de conexión a la base de datos
 if (!defined('DB_HOST')) {
-    define('DB_HOST', 'empresas.dnatech.com.ar');
 }
 if (!defined('DB_NAME')) {
-    define('DB_NAME', '{$empresa->nombre}');
 }
 if (!defined('DB_USER')) {
-    define('DB_USER', 'postgresql');
 }
 if (!defined('DB_PASSWORD')) {
-    define('DB_PASSWORD', 'lancelot');
 }
 // Ruta base del sitio
-define('BASE_URL', 'http://panel.casasantoni.com.ar');
 
 // Otras configuraciones
 define('APP_ENV', 'development');
@@ -28,10 +23,3 @@ if (APP_ENV === 'development') {
     ini_set('display_errors', 0);
     error_reporting(0);
 }
-
-// Configuraciones de la empresa
-// Configuraciones de la empresa
-define('EMPRESA_NOMBRE', "TIENDA");
-define('EMPRESA_DOMINIO', "casasantoni.com.ar");
-define('EMPRESA_LOGO', "logo1.png");
-define('IMP_URL', "https://panel.casasantoni.com.ar");
